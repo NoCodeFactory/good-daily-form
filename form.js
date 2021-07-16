fa6a12c3043ce74780d7393dd78548df38ece849
NoCodeFactory <87361683+NoCodeFactory@users.noreply.github.com>
2021-07-16 16:58:28 +0200

    test update function add

--- form.js
+++ form.js
@@ -161,14 +161,14 @@ const removeDnone = (element) => {
 
 // Adding the current price to the total price
 const addingToPrice = (currentPrice) => {
-    addedSubPrice = addedSubPrice += currentPrice
+    addedSubPrice = addedSubPrice + currentPrice
     subscriptionPrice.textContent = addedSubPrice.toFixed(2)
     singlePrice.textContent = parseInt(parseInt(addedSubPrice) / parseInt(nSalaries.value) * daysWorkMonth)
 }
 
 //  Deduct the current price from the total price
 const deductToPrice = (currentPrice) => {
-    addedSubPrice = addedSubPrice -= currentPrice
+    addedSubPrice = addedSubPrice - currentPrice
     subscriptionPrice.textContent = addedSubPrice.toFixed(2)
     singlePrice.textContent = parseInt(parseInt(addedSubPrice) / parseInt(nSalaries.value) * daysWorkMonth)
 }
