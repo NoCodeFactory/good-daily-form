7c2673a9daf12d0f3023612ba85754cd6f6b06af
NoCodeFactory <87361683+NoCodeFactory@users.noreply.github.com>
2021-07-19 15:02:48 +0200

    postalfield update

--- form.js
+++ form.js
@@ -223,7 +223,7 @@ form.addEventListener('keyup', () => {
         }
 })
 
-postalField.addEventListener('click', () => {
+postalField.addEventListener('keyup', () => {
     // Conditionnal visibility postal message
     if(parseInt((postalField.value).substring(0, 2)) != 75 && 92 && 93 && 94 && 95) {
         removeDnone(postalMessage)
