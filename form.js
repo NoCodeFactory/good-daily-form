f3115a8c3fcb08dad4cff671a2bf9070e0d76f86
NoCodeFactory <87361683+NoCodeFactory@users.noreply.github.com>
2021-08-27 17:03:23 +0200

    update commentaries

--- form.js
+++ form.js
@@ -4,7 +4,7 @@ const errorMessage = document.querySelector('#error-message')
 const submitFormButton = document.querySelector('#submit-form-button')
 
 	// Buttons variables
-let indexSlide = 0
+let indexSlide = 0 // Variable qui sert à savoir où en sommes-nous dans le formulaire
 const rightArrow = document.querySelector('#right-arrow')
 const nextButton = document.querySelector('#next-button')
 const backButton = document.querySelector('#back-button')
@@ -21,11 +21,13 @@ const messageServiceCheck = document.querySelector('#message-service-check')
 const postalMessage = document.querySelector('#postal-message')
 const seeAboResume = document.querySelector('#see-abo-resume')
 	
-  // Content resume variables
+  // Element du block résumé
 const aboResume = document.querySelector('#subscription-resume')
-let subscriptionPrice = document.querySelector('#subscription-price')
+let subscriptionPrice = document.querySelector('#subscription-price') // Element allant recevoir la valeur addedSubPrice.
 let singlePrice = document.querySelector('#single-price')
-let addedSubPrice = 0
+let addedSubPrice = 0 // valeur initiale du prix total
+
+// Sécurités relatives au non-spamming des boutons
 let securityPriceCoffee = false
 let securityPriceCoffeeGrain = false
 let securityPriceCoffeeCaps = false
@@ -35,20 +37,25 @@ let securityPriceTea = false
 let securityPriceSnacks = false
 let securityPriceFruits = false
 let securityPriceService = false
+
 const contentCoffee = document.querySelector('#content-coffee')
 let resumeCoffeePrice = document.querySelector('#resume-coffee-price')
 let resumeCoffeeText = document.querySelector('#resume-coffee-text')
+
 const contentTea = document.querySelector('#content-tea')
 let resumeTeaPrice = document.querySelector('#resume-tea-price')
 let resumeTeaText = document.querySelector('#resume-tea-text')
+
 const contentSnacks = document.querySelector('#content-snacks')
 let resumeSnacksPrice = document.querySelector('#resume-snacks-price')
 let resumeSnacksText = document.querySelector('#resume-snacks-text')
+
 const contentFruits = document.querySelector('#content-fruits')
 const contentFruitsDelivery = document.querySelector('#content-fruits-delivery')
 let resumeFruitsPrice = document.querySelector('#resume-fruits-price')
 let resumeFruitsText = document.querySelector('#resume-fruits-text')
 let resumeFruitsDay = document.querySelector('#resume-fruits-day')
+
 const contentService = document.querySelector('#content-service')
 let resumeServicePrice = document.querySelector('#resume-service-price')
 let resumeServiceSubOptionCoffee = document.querySelector('#resume-service-option-coffee')
@@ -61,35 +68,36 @@ let resumeServiceSubOptionDib = document.querySelector('#resume-service-option-d
 
 		// Coffee
 let subCoffee = document.querySelector('#sub-coffee')
+const subCoffeeOptions = document.querySelector('#sub-coffee_options') // Container des options
 const subCoffeeYes = document.querySelector('#sub-coffee-yes')
 const subCoffeeNo = document.querySelector('#sub-coffee-no')
-const subCoffeeOptions = document.querySelector('#sub-coffee_options')
 const coffeeGrain = document.querySelector('#coffee-grain')
+const coffeeCapsules = document.querySelector('#coffee-capsules')
 let coffeeGrainChecked = false
 let coffeeCapsChecked = false
-let machineYesChecked = false
-let machineNoChecked = false
-const coffeeCapsules = document.querySelector('#coffee-capsules')
-const grain = document.querySelector('#grain')
-const capsules = document.querySelector('#capsules')
 const addsCoffeeYes = document.querySelector('#adds-coffee-yes')
 const addsCoffeeNo = document.querySelector('#adds-coffee-no')
 const coffeeTypeContent = document.querySelector('#coffee-type-content')
 const machineContent = document.querySelector('#machine-content')
 const addsContent = document.querySelector('#adds-content')
+
+    // Variables CMS relatives au café
 let coffeeDayConsumption = parseFloat(document.querySelector('#coffee-day-consumption').textContent)
 let coffeePricePerKg = parseFloat(document.querySelector('#coffee-price-per-kg').textContent)
 let coffeeConsumptionPerkg = parseFloat(document.querySelector('#coffee-consumption-per-kg').textContent)
 let coffeePriceCaps = parseFloat(document.querySelector('#coffee-price-caps').textContent)
 let addsPrice = parseFloat(document.querySelector('#adds-price').textContent)
+
 let addsPriceTotal = 0
 let coffeeGrainPrice = 0
 let coffeeCapsPrice = 0
 let coffeePrice = 0
 
-    // Machine
+    // Machine café
 const machineYes = document.querySelector('#machine-yes')
 const machineNo = document.querySelector('#machine-no')
+let machineYesChecked = false
+let machineNoChecked = false
 let machineCoffeeS = parseFloat(document.querySelector('#machine-coffee-s').textContent)
 let machineCoffeeM = parseFloat(document.querySelector('#machine-coffee-m').textContent)
 let machineCoffeeL = parseFloat(document.querySelector('#machine-coffee-l').textContent)
@@ -98,64 +106,75 @@ let machinePrice = 0
 
 		// Tea
 let subTea = document.querySelector('#sub-tea')
+const subTeaOptions = document.querySelector('#sub-tea_options') // Container des options
 const subTeaYes = document.querySelector('#sub-tea-yes')
 const subTeaNo = document.querySelector('#sub-tea-no')
-const subTeaOptions = document.querySelector('#sub-tea_options')
 const teaVrac = document.querySelector('#tea-vrac')
 const teaSachets = document.querySelector('#tea-sachets')
+let teaPrice = 0
+
+    // Variables CMS relatives au thé
 let teaPercentageConsumption = parseFloat(document.querySelector('#tea-percentage-consumption').textContent)
 let teaDayConsumption = parseFloat(document.querySelector('#tea-day-consumption').textContent)
 let singleTeaPrice = parseFloat(document.querySelector('#single-tea-price').textContent)
-let teaPrice = 0
 
 		// Snacks
 let subSnacks = document.querySelector('#sub-snacks')
 const subSnacksYes = document.querySelector('#sub-snacks-yes')
 const subSnacksNo = document.querySelector('#sub-snacks-no')
-let snacksPricePerKg = parseFloat(document.querySelector('#snacks-price-per-kg').textContent)
 let snacksPriceTotal = 0
+
+    // Variable CMS relative aux snacks
+let snacksPricePerKg = parseFloat(document.querySelector('#snacks-price-per-kg').textContent)
 
 		// Fruits
 let subFruits = document.querySelector('#sub-fruits')
+const subFruitsOptions = document.querySelector('#sub-fruits_options') // Container des options
 const subFruitsYes = document.querySelector('#sub-fruits-yes')
 const subFruitsNo = document.querySelector('#sub-fruits-no')
-const subFruitsOptions = document.querySelector('#sub-fruits_options')
 const lundiRadioButton = document.querySelector('#fruits-lundi')
 const mardiRadioButton = document.querySelector('#fruits-mardi')
 const peuImporteRadioButton = document.querySelector('#fruits-peu-importe')
 let mondayCheck = document.querySelector('#monday-check')
 let tuesdayCheck = document.querySelector('#tuesday-check')
 let noneCheck = document.querySelector('#Peu-importe')
-let fruitsPricePerKg = parseFloat(document.querySelector('#fruits-price-per-kg').textContent)
 let fruitsPriceTotal = 0
+
+    // Variable CMS relative aux fruits
+let fruitsPricePerKg = parseFloat(document.querySelector('#fruits-price-per-kg').textContent)
 
 		// Service
 let subService = document.querySelector('#sub-service')
+const subSerivceOptions = document.querySelector('#sub-service_options') // Container des options
 const subServiceYes = document.querySelector('#sub-service-yes')
 const subServiceNo = document.querySelector('#sub-service-no')
-const subSerivceOptions = document.querySelector('#sub-service_options')
+let collectPriceTotal = 0
+
 const borneCoffee = document.querySelector('#born-coffee')
 let borneCoffeeCheck = document.querySelector('#born-coffee-check')
+
 const borneCaps = document.querySelector('#born-caps')
 let borneCapsCheck = document.querySelector('#born-caps-check')
+
 const bornePaperCardboard = document.querySelector('#born-paper-cardboard')
 let bornePaperCardboardCheck = document.querySelector('#born-paper-cardboard-check')
+
 const bornePlastic = document.querySelector('#born-plastic')
 let bornePlasticCheck = document.querySelector('#born-plastic-check')
+
 const borneCan = document.querySelector('#born-can')
 let borneCanCheck = document.querySelector('#born-can-check')
+
 const borneGlass = document.querySelector('#born-glass')
 let borneGlassCheck = document.querySelector('#born-glass-check')
+
 const borneDib = document.querySelector('#born-dib')
 let borneDibCheck = document.querySelector('#born-dib-check')
-let collectPrice = parseFloat(document.querySelector('#collect-price').textContent)
-let collectPriceTotal = 0
-let serviceSecurityPrice = false
 
-// Informations
-const emailForm = document.querySelector('#email-form')
-const nameStreetForm = document.querySelector('#name-street-form')
-const countryForm = document.querySelector('#country-form')
+    // Variable CMS relative au service
+let collectPrice = parseFloat(document.querySelector('#collect-price').textContent)
+
+// Informations slide 7 sur le code postal
 const postalField = document.querySelector('#postal-field')
 
 // Hidden fields
@@ -168,15 +187,14 @@ let oTotalPrice = document.querySelector('#o-total-price')
 let oSinglePrice = document.querySelector('#o-single-price')
 
 
-// Informations variables
-
+// Informations générales
 let nSalaries = document.querySelector('#n-salarie')
 let nSalariesPresents = 0
-const daysWorkYear = parseFloat(document.querySelector('#days-work-year').textContent)
+const daysWorkYear = parseFloat(document.querySelector('#days-work-year').textContent) // Variable CMS
 const daysWorkMonth = daysWorkYear / 12
 const jourTravail = document.querySelector('#jour-travail')
 const pointConso = document.querySelector('#point-conso')
-const addedCostFirstSubscribe = parseFloat(document.querySelector('#added-cost-first-subscribe').textContent)
+const addedCostFirstSubscribe = parseFloat(document.querySelector('#added-cost-first-subscribe').textContent) // Variable CMS
 
 // Functions add & remove d-none
 const addDnone = (element) => {
@@ -187,7 +205,7 @@ const removeDnone = (element) => {
 	element.classList.remove('d-none')
 }
 
-// Adding the current price to the total price
+// Ajout du prix donné au prix total
 const addingToPrice = (currentPrice) => {
     addedSubPrice = addedSubPrice + currentPrice
     subscriptionPrice.textContent = addedSubPrice.toFixed(2)
@@ -198,7 +216,7 @@ const addingToPrice = (currentPrice) => {
     }
 }
 
-//  Deduct the current price from the total price
+// Déduction du prix donné au prix total
 const deductToPrice = (currentPrice) => {
     addedSubPrice = addedSubPrice - currentPrice
     subscriptionPrice.textContent = addedSubPrice.toFixed(2)
@@ -212,9 +230,10 @@ const deductToPrice = (currentPrice) => {
 // Moving throught the form
 nextButton.addEventListener('click', () => {
     if(indexSlide < 1) {
-        nSalariesPresents = Math.round(parseFloat(nSalaries.value) * (5 - parseFloat(jourTravail.value)) / 5)
-  }
+        nSalariesPresents = Math.round(parseFloat(nSalaries.value) * (5 - parseFloat(jourTravail.value)) / 5) // Calcul de la valeur "salariés présents"
+    }
 
+    // Sert à passer à la slide 7 directement si le nombre de salariés entrées est supérieur ou égal à 100
     if(parseInt(nSalaries.value) >= 100) {
         if(parseInt(nSalaries.value) >= 100 && indexSlide == 0 && jourTravail.value != "" && pointConso.value != "") {
             indexSlide += 1
@@ -261,13 +280,14 @@ nextButton.addEventListener('click', () => {
         }
     } else {
 
+        // Slide 1 pas de résumé d'abonnement
         if(indexSlide === 0) {
             addDnone(seeAboResume)    
         } else {
             removeDnone(seeAboResume)
         }
     
-        // Conditionnal informations company
+        // Check des informations de l'entreprise
         if(indexSlide === 0 && nSalaries.value != "" && jourTravail.value != "" && pointConso.value != "") {
             rightArrow.click()
             indexSlide += 1
@@ -277,7 +297,7 @@ nextButton.addEventListener('click', () => {
             removeDnone(errorMessage)
         }
     
-        // Conditionnal Coffee
+        // Check des informations pour le café
         if(indexSlide === 1 && subCoffee.textContent === "Oui") {
             if(coffeeGrainChecked === true || coffeeCapsChecked === true) {
                 rightArrow.click()
@@ -294,7 +314,7 @@ nextButton.addEventListener('click', () => {
             removeDnone(errorMessage)
         }
     
-        // Conditionnal Tea
+        // Check des informations pour le thé
         if(indexSlide === 2 && subTea.textContent === "Oui") {
             if(resumeTeaText.textContent === "vrac" || resumeTeaText.textContent === "sachets") {
                 rightArrow.click()
@@ -309,7 +329,7 @@ nextButton.addEventListener('click', () => {
             removeDnone(errorMessage)
         }
     
-        // Conditionnal Snacks
+        // Check des informations pour le snacks
         if(indexSlide === 3 && subSnacks.textContent === "Oui") {
             rightArrow.click()
             indexSlide += 1
@@ -322,7 +342,7 @@ nextButton.addEventListener('click', () => {
             removeDnone(errorMessage)
         }
     
-        // Conditionnal Fruits
+        // Check des informations pour le fruits
         if(indexSlide === 4 && subFruits.textContent === "Oui") {
             if(resumeFruitsDay.textContent === "lundi" || resumeFruitsDay.textContent === "mardi" || resumeFruitsDay.textContent === "lundi ou mardi") {
                 rightArrow.click()
@@ -339,7 +359,7 @@ nextButton.addEventListener('click', () => {
             removeDnone(errorMessage)
         }
     
-        // Conditionnal Service
+        // Check des informations pour le service
         if(indexSlide === 5 && subService.textContent === "Oui") {
             if(borneCoffeeCheck.checked === true || borneCapsCheck.checked === true || bornePaperCardboardCheck.checked === true || bornePlasticCheck.checked === true || borneCanCheck.checked === true || borneGlassCheck.checked === true || borneDibCheck.checked === true) {
                 rightArrow.click()
@@ -362,6 +382,7 @@ nextButton.addEventListener('click', () => {
         }
     }
 
+    // Remplissage des hidden fields
     if(indexSlide === 6) {
         addDnone(nextButton)
         removeDnone(submitFormButton)
@@ -376,6 +397,7 @@ nextButton.addEventListener('click', () => {
   }
 })
 
+// d-none sur le back button
 if(indexSlide == 0) {
     addDnone(backButton)
 }
@@ -403,17 +425,17 @@ backButton.addEventListener('click', () => {
     addDnone(errorMessage)
 })
 
-// Taking informations from the form
+// Listener sur le formulaire quand la personne écrit
 form.addEventListener('keyup', () => {
 
-    // Conditionnal visibility commercial contact
+    // Message conditionnel pour les demandes hors-scopes
     if(parseInt(nSalaries.value) >= 101) {
         removeDnone(commercialContact)
     } else {
         addDnone(commercialContact)
     }
 
-    // Conditionnal visibility postal message
+    // Message conditionnel relatif aux codes postaux où gooddaily peut livrer
     if((postalField.value).length > 2) {
         if((postalField.value).substring(0, 2) != "75" && (postalField.value).substring(0, 2) != "92" && (postalField.value).substring(0, 2) != "93" && (postalField.value).substring(0, 2) != "94" && (postalField.value).substring(0, 2) != "95") {
             removeDnone(postalMessage)
@@ -444,8 +466,8 @@ pointConso.addEventListener('click', () => {
     }
 })
 
-// Coffee functions
 
+// Coffee functions
 subCoffeeYes.addEventListener('click', () => {
     removeDnone(contentCoffee)
     removeDnone(subCoffeeOptions)
@@ -460,17 +482,17 @@ subCoffeeYes.addEventListener('click', () => {
         securityPriceCoffee = true
     }
 
-    subCoffeeNoChecked = false
+    subCoffeeNoChecked = false // Prend l'information pour savoir si "oui" ou "non" est coché
 })
 
 subCoffeeNo.addEventListener('click', () => {
-    subCoffeeNoChecked = true
+    subCoffeeNoChecked = true // Prend l'information pour savoir si "oui" ou "non" est coché
 
     addDnone(contentCoffee)
     addDnone(subCoffeeOptions)
     subCoffee.textContent = "Non"
 
-    // Not giving wrong informations in the form post
+    // Réinitialisation des informations si l'utilisateur avait coché "oui"
     machinePrice = 0
     addsPriceTotal = 0
     machineNo.click()
@@ -503,6 +525,7 @@ subCoffeeNo.addEventListener('click', () => {
     securityPriceCoffeeAdds = false
 })
 
+// Calcul du coup de la machine selon le Gdoc de Gooddaily
 const machinePriceCalculation = () => {
     if(nSalariesPresents <= 30) {
     
@@ -592,6 +615,7 @@ coffeeGrain.addEventListener('click', () => {
     // Calculate the price
     coffeeGrainPrice = nSalariesPresents * coffeeDayConsumption * daysWorkMonth * (coffeePricePerKg / coffeeConsumptionPerkg)
 
+    // Recalcule si des options ont été cochées avant
     if(machineYesChecked === true && securityPriceCoffeeGrain === false && securityPriceCoffeeAdds === false) {
       deductToPrice(coffeeCapsPrice)
       coffeeCapsPrice = 0
@@ -653,7 +677,7 @@ coffeeGrain.addEventListener('click', () => {
         securityPriceCoffeeCaps = false
     }
     
-    // Security for not spamming
+    // Sécurité pour ne plus spammer
     if(securityPriceCoffeeGrain === false) {
 
         deductToPrice(coffeeCapsPrice)
@@ -680,6 +704,7 @@ coffeeCapsules.addEventListener('click', () => {
     // Calculate the price
     coffeeCapsPrice = nSalariesPresents * coffeeDayConsumption * daysWorkMonth * coffeePriceCaps
 
+    // Recalcule si des options ont été cochées avant
     if(machineYesChecked === true && securityPriceCoffeeCaps === false && securityPriceCoffeeAdds === false) {
         deductToPrice(coffeeGrainPrice)
         coffeeGrainPrice = 0
@@ -823,7 +848,6 @@ addsCoffeeNo.addEventListener('click', () => {
 })
 
 // Tea functions
-
 subTeaYes.addEventListener('click', () => {
     removeDnone(contentTea)
     removeDnone(subTeaOptions)
@@ -858,8 +882,8 @@ teaSachets.addEventListener('click', () => {
 	resumeTeaText.textContent = "sachets"
 })
 
-// Snacks functions
 
+// Snacks functions
 subSnacksYes.addEventListener('click', () => {
     removeDnone(contentSnacks)
     subSnacks.textContent = "Oui"
@@ -992,6 +1016,7 @@ subServiceYes.addEventListener('click', () => {
     removeDnone(subSerivceOptions)
     subService.textContent = "Oui"
 
+    // Calcul du prix du service selon le nombre de point de collecte
     if(nSalariesPresents >= 0 && nSalariesPresents <= 30) {
         if(pointConso.value === "1") {
             collectPriceTotal = collectPrice * 1
@@ -1025,7 +1050,7 @@ subServiceYes.addEventListener('click', () => {
         resumeServicePrice.textContent = collectPriceTotal
     }
 
-    // If coffee grain was choosed
+    // Pre-check si café grain a été pris
     if(coffeeGrainChecked === true) {
         borneCoffee.click()
         borneCoffeeCheck.disabled = true
@@ -1033,7 +1058,7 @@ subServiceYes.addEventListener('click', () => {
 	    removeDnone(resumeServiceSubOptionCoffee)
     }
 
-    // If coffee capsules was choosed
+    // Pre-check si café capsule a été pris
     if(coffeeCapsChecked === true) {
         borneCaps.click()
         borneCapsCheck.disabled = true
@@ -1041,6 +1066,7 @@ subServiceYes.addEventListener('click', () => {
 	    removeDnone(resumeServiceSubOptionCaps)
     }
 
+    // Sécurité pour le spam de bouton
     if(securityPriceService === false) {
         addingToPrice(collectPriceTotal)
         securityPriceService = true
@@ -1094,6 +1120,7 @@ subServiceNo.addEventListener('click', () => {
     }
 })
 
+// Listener des bandes de collecte
 borneCoffee.addEventListener('click', () => {
     borneCoffeeCheck.checked = !borneCoffeeCheck.checked
 
